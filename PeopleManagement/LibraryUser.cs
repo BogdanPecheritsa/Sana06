f2982988008a88372fb83de4cd3cb889bc417c3d
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-29 16:20:49 +0200

    Added the LibraryUser class, which implements properties and a constructor

---
+++ PeopleManagement/LibraryUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleManagement
+{
+    public class LibraryUser : Person
+    {
+        public byte LibraryCardNumber { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public double MonthlyLibraryFee { get; set; }
+
+        public LibraryUser(string name, string surname, DateTime dateOfBirth, byte libraryCardNumber, DateTime dateOfIssue,
+            double monthlyLibraryFee) : base(name, surname, dateOfBirth)
+        {
+            LibraryCardNumber = libraryCardNumber;
+            DateOfIssue = dateOfIssue;
+            MonthlyLibraryFee = monthlyLibraryFee;
+        }
+    }
+}
