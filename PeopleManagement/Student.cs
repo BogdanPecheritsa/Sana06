03d362c5f69a11ae57092ec78d5a60cccbefe45e
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-29 16:06:48 +0200

    Added the Student class, which implements properties and a constructor

---
+++ PeopleManagement/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleManagement
+{
+    public class Student : Applicant
+    {
+        public byte Course { get; set; }
+        public byte Group { get; set; }
+        public string Faculty { get; set; }
+        public string HigherEducationInstitution { get; set; }
+
+        public Student(string name, string surname, DateTime dateOfBirth, byte znoPoints, byte educationPoints, string educationInstitutionName,
+        byte course, byte group, string faculty, string higherEducationInstitution) : base(name, surname, dateOfBirth, znoPoints,
+            educationPoints, educationInstitutionName)
+        {
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+            HigherEducationInstitution = higherEducationInstitution;
+        }
+    }
+}
