d3b1f4de91f23c6ef8eb7ad97ede6e8bc41c6d95
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-29 15:50:29 +0200

    Added the Applicant class, which implements properties and a constructor.

---
+++ PeopleManagement/Applicant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleManagement
+{
+    public class Applicant : Person
+    {
+        public byte ZnoPoints { get; set; }
+        public byte EducationPoints { get; set; }
+        public string EducationInstitutionName { get; set; }
+
+        public Applicant(string name, string surname, DateTime dateOfBirth, byte znoPoints, byte educationPoints, string educationInstitutionName)
+            : base(name, surname, dateOfBirth)
+        {
+            ZnoPoints = znoPoints;
+            EducationPoints = educationPoints;
+            EducationInstitutionName = educationInstitutionName;
+        }
+    }
+}
