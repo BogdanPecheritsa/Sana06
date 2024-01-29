38bb1b823e1996de561ae34bdb3c7fb55a63185a
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-29 16:15:35 +0200

    Added the Teacher class, which implements properties and a constructor

---
+++ PeopleManagement/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleManagement
+{
+    public class Teacher : Person
+    {
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string HigherEducationInstitution { get; set; }
+
+        public Teacher(string name, string surname, DateTime dateOfBirth, string higherEducationInstitution, string position, string department)
+            : base(name, surname, dateOfBirth)
+        {
+            Position = position;
+            Department = department;
+            HigherEducationInstitution = higherEducationInstitution;
+        }
+    }
+}
